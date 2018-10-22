60301fc0f7c9a0b31c04e354c9b6fd637019211a
Marvin Scholz <epirat07@gmail.com>
2018-10-22 12:45:16 +0200

    Decklink SDK 10.9.11

--- Mac/include/DeckLinkAPIVersion.h
+++ Mac/include/DeckLinkAPIVersion.h
@@ -30,8 +30,8 @@
 #ifndef __DeckLink_API_Version_h__
 #define __DeckLink_API_Version_h__
 
-#define BLACKMAGIC_DECKLINK_API_VERSION					0x0a090a00
-#define BLACKMAGIC_DECKLINK_API_VERSION_STRING			"10.9.10"
+#define BLACKMAGIC_DECKLINK_API_VERSION					0x0a090b00
+#define BLACKMAGIC_DECKLINK_API_VERSION_STRING			"10.9.11"
 
 #endif	// __DeckLink_API_Version_h__
 
